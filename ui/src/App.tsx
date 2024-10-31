01a4fc2210dab32a666b2cf7070911c1167c511c
Sebastian Łuczak <sebastian.m.luczak@gmail.com>
2024-10-31 18:13:00 +0100

    Some adjustments to UI.

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -7,6 +7,13 @@ type JsonResponse = {
   wrongPhotos: string[];
 };
 
+type Riddle = {
+  word: string;
+  photo: string;
+  wrongPhotos: string[];
+  isCorrect: boolean | null;
+};
+
 const categories = ['food', 'animals'];
 
 function App() {
@@ -19,14 +26,21 @@ function App() {
   const [correctGuesses, setCorrectGuesses] = useState(0);
   const [incorrectGuesses, setIncorrectGuesses] = useState(0);
   const [readTheDocs, setReadTheDocs] = useState<string>("");
+  const [riddleHistory, setRiddleHistory] = useState<Riddle[]>([]);
 
   const fetchDataFromBackend = async () => {
+    if (wordToGuess && photoToGuess) {
+      setRiddleHistory(prevHistory => [
+        { word: wordToGuess, photo: photoToGuess, wrongPhotos, isCorrect },
+        ...prevHistory,
+      ]);
+    }
+
     setReadTheDocs("");
     setData("Losuję nowy wyraz...");
     setWrongPhotos([]);
 
     const response = await fetch(`http://localhost:8080/words/get?category=${currentCategory}`);
-    console.log(`After response of ${currentCategory}`);
     if (response.ok) {
       const json = await response.json() as JsonResponse;
 
@@ -76,12 +90,18 @@ function App() {
         <span className="emphasized incorrect">Nieprawidłowo: {incorrectGuesses}</span>
       </div>
       <h1>🧑‍🏫 Learn English</h1>
-      <h2>Ucz się języka angielskiego</h2>
 
       <div className="main-container">
         <div className="left-column">
+          <button
+            onClick={fetchDataFromBackend}
+            className={isCorrect ? 'explode' : ''}
+          >
+            <h2>Losuj nowy wyraz</h2>
+          </button>
+
           <div className="category-selector">
-            <label htmlFor="category">Wybierz kategorię:</label>
+            <label htmlFor="category">Zmień kategorię:</label>
             <select id="category" value={currentCategory} onChange={handleCategoryChange}>
               {categories.map((category) => (
                 <option key={category} value={category}>
@@ -91,13 +111,25 @@ function App() {
             </select>
           </div>
 
-          <button onClick={fetchDataFromBackend}>
-            <h2>Zgadnij nowy wyraz w języku angielskim</h2>
-          </button>
-
-          <p className="read-the-docs">
+          <p className={isCorrect ? 'read-the-docs explode' : 'read-the-docs'}>
             <h2>{readTheDocs}</h2>
           </p>
+
+          <div className="riddle-history">
+            <h3>Historia:</h3>
+            <ul>
+              {riddleHistory.map((riddle, index) => (
+                <ol key={index}>
+                  <strong>{riddle.word.toUpperCase()}</strong> - Correct: {riddle.isCorrect ? 'Yes' : 'No'}
+                  <div className="history-images">
+                    {riddle.wrongPhotos.map((photo, idx) => (
+                      <img key={idx} src={photo} alt={`History ${index} photo ${idx}`} width="50"/>
+                    ))}
+                  </div>
+                </ol>
+              ))}
+            </ul>
+          </div>
         </div>
 
         <div className="right-column">
@@ -109,7 +141,7 @@ function App() {
                 className={`grid-item ${selectedPhoto === photo ? (isCorrect ? 'correct' : 'incorrect') : ''}`}
                 onClick={() => handlePhotoClick(photo)}
               >
-                <img src={photo} alt={`Guess ${index}`} />
+                <img src={photo} alt={`Guess ${index}`}/>
               </button>
             ))}
           </div>
